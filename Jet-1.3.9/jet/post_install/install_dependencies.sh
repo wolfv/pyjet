366a4a644c6c201fe41e568c164557880959318b
Orestis Zambounis <orestis.zambounis@gmail.com>
2017-03-25 21:30:26 +0100

    rm unused folder

--- Jet-1.3.9/jet/post_install/install_dependencies.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-apt-get install libblas-dev liblapack-dev # BLAS and LAPACK
-apt-get install python-pygraphviz # PyGraphviz
-apt-get install cmake
--- Jet-1.3.9/jet/thirdparty/cppimport/cppimport/__init__.py
+++
@@ -1,1 +0,0 @@
-from cppimport.import_hook import set_quiet, force_rebuild
